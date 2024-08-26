739e215221400e2dafa46ee9991d57e917d83759
zokevlar98 <zqouri11@gmail.com>
2024-08-26 11:11:05 +0100

    ft_uslep Mon Aug 26 11:11:05 +01 2024

---
+++ philo/mandatory/ft_usleep.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_usleep.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zqouri <zqouri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/26 10:29:53 by zqouri            #+#    #+#             */
+/*   Updated: 2024/08/26 11:06:41 by zqouri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+unsigned long get_time_now(void)
+{
+    struct timeval  t;
+
+    if (gettimeofday(&t, NULL) == -1)
+    {
+        printf("Error: gettimeofday\n");
+        return (0);
+    }
+    return ((t.tv_sec * 1e3) + (t.tv_usec / 1e3));
+}
+
+void    ft_usleep(unsigned long time)
+{
+    unsigned long range;
+
+    range = get_time_now();
+    if (range == 0)
+        return ;
+    while (get_time_now() < range + time)
+        usleep(100);
+    return ;
+}
